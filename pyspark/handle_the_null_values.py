72a71655449b79c3f3aabd83ec219c98a98f446a
Sathya112716 <sathyapriya112716@gmail.com>
2024-06-21 22:49:01 +0530

    This folder conbtains pyspark code

--- pyspark/handle_the_null_values.py
+++ pyspark/handle_the_null_values.py
@@ -1,4 +1,4 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession #
 from pyspark.sql.functions import *
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
