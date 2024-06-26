ea8921f067741394c744c183f6a7a09fc31a045c
Sathya112716 <sathyapriya112716@gmail.com>
2024-06-26 23:03:24 +0530

    Lambda functions examples

--- python/functions_1.py
+++ python/functions_1.py
@@ -1,4 +1,4 @@
-# Example 1: Adding two numbers using a lambda function
+# Example 1: Adding two numbers using a lambda function:
 add = lambda x, y: x + y
 result = add(3, 5)
 print("Result of addition:", result)
